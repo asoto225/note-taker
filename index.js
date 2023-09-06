536d1481ca48a3abfb4ab98e36b1dcb8a006a1b9
asoto225 <135178400+abel25xD@users.noreply.github.com>
2023-09-05 18:31:18 -0700

    changed routes so that code works

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 const express = require('express');
+const { writeFile } = require('fs/promises')
 const path = require('path');
 const noteData = require('./db/db.json');
+const { v4: uuidv4 } = require('uuid');
 const PORT = 3001;
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get('/*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-})
-
 app.get ('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
-})
+});
 
-app.get('/api/notes', (req,res) => res.json(noteData));
+app.get('/api/notes', (req,res) => {
+    res.json(noteData);
+});
 
 
 app.post('/api/notes', (req, res) => {
-    res.json(`${req.method} request received to add a note`);
-    console.info(req.rawHeaders);
-    console.info(`hello world`);
+    const newPost = req.body
+    newPost.id = uuidv4()
+    noteData.push(newPost)
+    writeFile('db/db.json', JSON.stringify(noteData))
+    res.json(noteData)
 });  
 
 app.listen(PORT, () => {
